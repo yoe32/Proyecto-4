20bcb235f2e050a878d76f89c07c4fa6041ee94d
yoe32 <yoe32ns@hotmail.com>
2016-08-21 15:59:10 -0600

    yohel_crap_23

---
+++ SteelArena/Assets/Assets/Script/YohelScripts/CameraOrbit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrbit : MonoBehaviour
+{
+	public GameObject target = null;
+	public bool orbitY = false;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(target != null)
+		{
+			transform.LookAt (target.transform);
+
+			if (orbitY) 
+			{
+				transform.RotateAround (target.transform.position, Vector3.up, Time.deltaTime * 5);
+			}
+		}
+	}
+}
