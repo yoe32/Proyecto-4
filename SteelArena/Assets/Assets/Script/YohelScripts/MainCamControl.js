b891368ac823dcbc5c161f3fc4347ea2a9cb2d53
yoe32 <yoe32ns@hotmail.com>
2016-08-21 03:26:24 -0600

    yohel_crap_19

---
+++ SteelArena/Assets/Assets/Script/YohelScripts/MainCamControl.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var currentMount : Transform;
+public var speedFactor : float = 0.1;
+
+function Start () {
+
+}
+
+function Update () {
+transform.position = Vector3.Lerp(transform.position,currentMount.position,speedFactor);
+transform.rotation = Quaternion.Slerp(transform.rotation,currentMount.rotation,speedFactor);
+}
+
+function setMount(newMount : Transform){
+currentMount = newMount;
+}
